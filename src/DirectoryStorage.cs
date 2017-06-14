9a64ef91fb060b89e723a2c50fee22787974bf47
Victor Milovanov <lost@losttech.software>
2017-06-14 00:12:57 -0700

    added key encoding

--- src/DirectoryStorage.cs
+++ src/DirectoryStorage.cs
@@ -2,7 +2,10 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Globalization;
     using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
     using System.Threading.Tasks;
 
     public sealed class DirectoryStorage : IWriteableKeyValueStore<string, byte[]>
@@ -15,22 +18,27 @@
 
         string GetFullPath(string key)
         {
+            key = KeyEncode(key);
+            // encode final directory separator, if any
+            char lastChar = key[key.Length - 1];
+            if (Path.DirectorySeparatorChar == lastChar || Path.AltDirectorySeparatorChar == lastChar)
+                key = key.Substring(0, key.Length - 1) + $"%{(byte)lastChar:x2}";
             // TODO: SECURITY: ensure key points within directory
             return Path.Combine(this.directory.FullName, key);
         }
 
         public Task<bool?> Delete(string key)
         {
-            string fullPath = GetFullPath(key);
+            string fullPath = this.GetFullPath(key);
             bool existed = File.Exists(fullPath);
             if (existed)
-                File.Delete(GetFullPath(key));
+                File.Delete(this.GetFullPath(key));
             return Task.FromResult((bool?)existed);
         }
 
         public Task<byte[]> Get(string key)
         {
-            string fullPath = GetFullPath(key);
+            string fullPath = this.GetFullPath(key);
             try
             {
                 return Task.FromResult(File.ReadAllBytes(fullPath));
@@ -43,7 +51,7 @@
 
         public Task Put(string key, byte[] value)
         {
-            string fullPath = GetFullPath(key);
+            string fullPath = this.GetFullPath(key);
             Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
             File.WriteAllBytes(fullPath, value);
             return Task.CompletedTask;
@@ -51,14 +59,27 @@
 
         public Task<(bool, byte[])> TryGet(string key)
         {
-            string fullPath = GetFullPath(key);
+            string fullPath = this.GetFullPath(key);
             if (!File.Exists(fullPath))
                 return Task.FromResult((false, default(byte[])));
             try {
                 return Task.FromResult((true, File.ReadAllBytes(fullPath)));
-            } catch (FileNotFoundException e) {
+            } catch (FileNotFoundException) {
                 return Task.FromResult((false, default(byte[])));
             }
         }
+
+        static readonly Regex UnsupportedKeyCharRegex = new Regex(
+            $"[{string.Join("", Path.GetInvalidPathChars().Concat(new []{'%'}).Select(c => "\\"+c))}]",
+            RegexOptions.Compiled | RegexOptions.CultureInvariant);
+        static readonly Regex UnsupportedEscapedKeyCharRegex = new Regex(@"\%[a-f0-9]{2}", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+        public static string KeyEncode(string key) =>
+            string.IsNullOrEmpty(key)
+                ? throw new ArgumentNullException(nameof(key))
+                : UnsupportedKeyCharRegex.Replace(key, badMatch => $"%{(byte)badMatch.Value[0]:x2}");
+        public static string KeyDecode(string key) =>
+            string.IsNullOrEmpty(key)
+                ? throw new ArgumentNullException(nameof(key))
+                : UnsupportedEscapedKeyCharRegex.Replace(key, badMatch => ((char)Byte.Parse(badMatch.Value.Substring(1), NumberStyles.HexNumber)).ToString());
     }
 }
